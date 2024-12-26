8b64dbc9609d646d776901ac29a098455d8263b3
Stakheev Dmitrii <Dmitriy.Stakheev@softline.com>
2024-12-26 16:33:14 +0500

    Исправление подгрузки зависимостей. - `FileRelationProvider`:     - Добавлена переменная, для хранения неподгружаемых файлов     - Добавлена функция `cleanConditionalName` для очистки названия файла     - Изменён `conditionalLoadAst` для очистки и проверки названия файла - `LoginPanel`:     - Подправлена функция `onDidReceiveMessage`

--- src/modules/ConnectionPanel/LoginPanel.ts
+++ src/modules/ConnectionPanel/LoginPanel.ts
@@ -13,9 +13,8 @@ export class LoginPanel extends GenericWebViewPanel {
             case 'login':
                 try {
                     let connectionInfo = new ConnectionInfo(message.connectionInfo.url, message.connectionInfo.login, message.connectionInfo.password);
-                    if (connectionInfo.getHostName() === '') {
-                        throw new Error("Unable to parse url. Example: http://localhost:5000");
-                    } else if (connectionInfo.getProtocol() !== 'http:' && connectionInfo.getProtocol() !== 'https:') {
+                    
+                    if (connectionInfo.getProtocol() !== 'http:' && connectionInfo.getProtocol() !== 'https:') {
                         throw new Error("Unsupported protocol");
                     }
 
@@ -29,6 +28,9 @@ export class LoginPanel extends GenericWebViewPanel {
                     if (error.code === 'ECONNREFUSED') {
                         vscode.window.showErrorMessage(`Couldn't connect to ${error.address}:${error.port}`);
                     }
+                    if (error.code === "ERR_INVALID_URL") {
+                        vscode.window.showErrorMessage("Unable to parse url. Example: http://localhost:5000");
+                    }   
                     else {
                         vscode.window.showErrorMessage(error.message);
                     }
